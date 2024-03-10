d6e1f839d928c4b2b9ae093d0bb1370cb9516b6e
larytet <>
2024-03-10 17:15:52 +0200

    Overseerr

--- overseerr.py
+++ overseerr.py
@@ -1,24 +1,38 @@
+# Usage:
+# python3 ./overseerr.py --api_key MT............== --overseerr_url http://localhost:5055
+
 import requests
 import argparse
+from collections import namedtuple
+
+Request = namedtuple('Request', ['id', 'type', 'status', 'media_id', 'media_type', 'requested_by'])
+
 
 def get_overseerr_requests(api_key, overseerr_url):
-    """Fetches requested items from Overseerr."""
-    headers = {
-        "X-Api-Key": api_key,
-    }
+    """Fetches requested items from Overseerr and returns them as a list of named tuples."""
+    headers = {"X-Api-Key": api_key}
 
     try:
         response = requests.get(f"{overseerr_url}/api/v1/request", headers=headers)
         response.raise_for_status()
-        print(response.text)
 
-        # Assuming the data is in JSON format and contains a list of requests
-        requests_data = response.json()
-        for request in requests_data:
-            print(f"Requested Item: {request['title']} by {request['requestedBy']}")
+        data = response.json()
+        requests_list = [
+            Request(
+                id=request['id'],
+                type=request['type'],
+                status=request['status'],
+                media_id=request['media']['id'],
+                media_type=request['media']['mediaType'],
+                requested_by=request['requestedBy']['plexUsername']
+            )
+            for request in data['results']
+        ]
+        return requests_list
 
     except requests.RequestException as e:
         print(f"Error fetching data from Overseerr: {e}")
+        return []
 
 
 def  parse_args():
@@ -32,7 +46,8 @@ def  parse_args():
 
 def main():
     args = parse_args()
-    get_overseerr_requests(args.api_key, args.overseerr_url)
+    requests = get_overseerr_requests(args.api_key, args.overseerr_url)
+    print(requests)
 
 if __name__ == "__main__":
     main()
